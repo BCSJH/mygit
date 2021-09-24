7577f620561a25b3b7a0a30568cd4ae1eae6f935
BCSJH <kimbh666@naver.com>
2021-09-24 16:22:05 +0900

    안녕3 추가

--- 01_test/src/kh/com/main.java
+++ 01_test/src/kh/com/main.java
@@ -4,8 +4,9 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("안녕");
+		System.out.println("안녕1");
 		System.out.println("안녕2");
+		System.out.println("안녕3");
 	}
 
 }
