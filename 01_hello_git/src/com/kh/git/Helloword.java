600b05c1399fab3a3b84e49b07c579fcee0ad99a
BCSJH <kimbh666@naver.com>
2021-09-24 10:45:14 +0900

    say Foo 기능 추가

--- 01_hello_git/src/com/kh/git/Helloword.java
+++ 01_hello_git/src/com/kh/git/Helloword.java
@@ -1,11 +1,14 @@
 package com.kh.git;
 
+import com.kh.foo.Foo;
+
 public class Helloword {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world~");
 		System.out.println("Hello git~");
+		new Foo().sayFoo();
 	}
 
 }
